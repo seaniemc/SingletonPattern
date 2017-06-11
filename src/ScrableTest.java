3873c55fe8f0a9dc122f821f73aaad6faef6da89
Sean McGrath <Sean McGrath>
2017-06-11 17:30:53 +0100

    created tests for scrabletest

--- src/ScrableTest.java
+++ src/ScrableTest.java
@@ -1,5 +1,28 @@
+import java.util.LinkedList;
+
 /**
  * Created by Sean on 11/06/2017.
  */
 public class ScrableTest {
+
+    public static void main(String[] args) {
+
+        Singleton newInstance = Singleton.getinstance();
+        System.out.println("Instance 1 ID: = [" + System.identityHashCode(newInstance) + "]");
+        System.out.println(newInstance.getLetterList());
+
+        LinkedList<String> playerOneTiles = newInstance.getTiles(7);
+        System.out.println("Player 1:" + playerOneTiles);
+
+        System.out.println(newInstance.getLetterList());
+
+        Singleton secondInstance = Singleton.getinstance();
+        System.out.println("Instance 2 ID: = [" + System.identityHashCode(secondInstance) + "]");
+        System.out.println(secondInstance.getLetterList());
+
+        LinkedList<String> playerTwoTiles = newInstance.getTiles(7);
+        System.out.println("Player 2:" + playerTwoTiles);
+
+        System.out.println(newInstance.getLetterList());
+    }
 }
--- src/Singleton.java
+++ src/Singleton.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 
 public class Singleton {
@@ -35,6 +36,8 @@ public class Singleton {
                 }
             }
             firstInstance = new Singleton();
+
+            Collections.shuffle(firstInstance.letterList);
         }
 
         return firstInstance;
